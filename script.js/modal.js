37435ef1fd9583ea7ffd9f1819d66411488e3582
Богдана Гостинська <111461462+banadana@users.noreply.github.com>
2022-10-12 01:03:38 +0300

    Add files via upload

--- script.js/modal.js
+++ script.js/modal.js
@@ -2,10 +2,7 @@ $('.header__tube ').on('click',function(){
     $(this).fadeOut();
 }),
 
-$('#open-tube').on('click',function(){
-    $('.header__tube').fadeIn();
-})
 
-$('#open--tube').on('click',function(){
+$('#open-tube').on('click',function(){
     $('.header__tube').fadeIn();
 })
