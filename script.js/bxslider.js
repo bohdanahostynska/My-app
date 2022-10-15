b7941aea4cd8058c403fb15ca87d8bb1e7559ed8
Богдана Гостинська <111461462+banadana@users.noreply.github.com>
2022-10-16 00:46:19 +0300

    Add files via upload

---
+++ script.js/bxslider.js
@@ -0,0 +1,17 @@
+const feedback = $(".feedback__list") .bxSlider ( {
+    pager:false,
+    controls:false,
+    infiniteLoop:true,
+    touchEnabled:true,
+    hideControlOnEnd:false,
+});
+
+$('.button--prev') .click((e)=>{
+    e.preventDefault(),
+   feedback.goToPrevSlide()
+});
+
+$('.button--next') .click((e)=>{
+    e.preventDefault(),
+    feedback.goToNextSlide()
+})
--- script.js/script.js
+++ script.js/script.js
@@ -1,7 +1,10 @@
 $ ('.header__ul > li') .on ('click',function(){
     $(this).children('.header__icon').toggleClass('header-rotate-icon');
+});
+
+$ ('.question__list > div') .on ('click',function(){
+    $(this).children('.question__icon').toggleClass('question-form-icon');
+    $(this).next('.question__text').slideToggle(300);
 })
-
-
 
 
