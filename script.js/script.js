f57f1956c308e859e1b0e2b40a2cb1605c62333b
Богдана Гостинська <111461462+banadana@users.noreply.github.com>
2022-10-23 15:22:16 +0300

    Add files via upload

--- script.js/script.js
+++ script.js/script.js
@@ -1,5 +1,6 @@
 $ ('.header__ul > li') .on ('click',function(){
     $(this).children('.header__icon').toggleClass('header-rotate-icon')
+    onclick="this.classList.toggle('open')"
 });
 
 $ ('.question__items>div') .on ('click',function(){
